7e7583e4e5d43b4c761ff661c7944c27dedb4e6a
Jamshidbek <jamanu1910077@gmail.com>
2023-01-28 15:54:43 +0500

    searche engine fixed

--- route/movies/searchEngine.js
+++ route/movies/searchEngine.js
@@ -19,108 +19,115 @@ const MiniSearch = require("minisearch");
 //   });
 // });
 route.get("/search", async (req, res) => {
-  const { value } = req.body;
-  let miniSearch = new MiniSearch({
-    fields: ["title", "overview"], // fields to index for full-text search
-    idField: "_id",
-    storeFields: [
-      "_id",
-      "title",
-      "backdropPath",
-      "overview",
-      "releaseDate",
-      "voteAverage",
-      "movie_id",
-    ], // fields to return with search results
-  });
-  mongoose.connect(config.mongoDB, async (err, db) => {
-    if (err) {
-      console.log(`Error accuried: ${err}`);
-    } else {
-      await db
-        .collection("action_movies")
-        .find({})
-        .toArray(async function (err, result) {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("cartoon_movies")
-        .find({})
-        .toArray(async function (err, result) {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("fantasy_movies")
-        .find({})
-        .toArray(async function (err, result) {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("cartoon_movies")
-        .find({})
-        .toArray(async (err, result) => {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("popular_movies")
-        .find({})
-        .toArray(async (err, result) => {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("science_fiction_movies")
-        .find({})
-        .toArray(async (err, result) => {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("trending_movies")
-        .find({})
-        .toArray(async (err, result) => {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-          }
-        });
-      await db
-        .collection("war_movies")
-        .find({})
-        .toArray(async (err, result) => {
-          if (err) {
-            res.send(err);
-          } else {
-            miniSearch.addAll(result);
-            const searchedMovie = await miniSearch.search(value);
-            res.send(searchedMovie);
-          }
-        });
+  try {
+    const { value } = req.query;
+    if (!value) {
+      return res.send("Quey Required");
     }
-  });
+    let miniSearch = new MiniSearch({
+      fields: ["title", "overview"], // fields to index for full-text search
+      idField: "_id",
+      storeFields: [
+        "_id",
+        "title",
+        "backdropPath",
+        "overview",
+        "releaseDate",
+        "voteAverage",
+        "movie_id",
+      ], // fields to return with search results
+    });
+    mongoose.connect(config.mongoDB, async (err, db) => {
+      if (err) {
+        console.log(`Error accuried: ${err}`);
+      } else {
+        await db
+          .collection("action_movies")
+          .find({})
+          .toArray(async function (err, result) {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("cartoon_movies")
+          .find({})
+          .toArray(async function (err, result) {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("fantasy_movies")
+          .find({})
+          .toArray(async function (err, result) {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("cartoon_movies")
+          .find({})
+          .toArray(async (err, result) => {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("popular_movies")
+          .find({})
+          .toArray(async (err, result) => {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("science_fiction_movies")
+          .find({})
+          .toArray(async (err, result) => {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("trending_movies")
+          .find({})
+          .toArray(async (err, result) => {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+            }
+          });
+        await db
+          .collection("war_movies")
+          .find({})
+          .toArray(async (err, result) => {
+            if (err) {
+              res.send(err);
+            } else {
+              miniSearch.addAll(result);
+              const searchedMovie = await miniSearch.search(value);
+              res.send(searchedMovie);
+            }
+          });
+      }
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 module.exports = route;
